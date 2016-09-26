31103709320c7e58eb958726b3fca3b0c3bcdd20
jangrakul <jangra.kuldeep@gmail.com>
2016-09-26 11:17:03 +0100

    Added hello world example for Angular

---
+++ web-module/src/main/app/main.ts
@@ -0,0 +1,7 @@
+// Import modules
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
